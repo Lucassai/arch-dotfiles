27f4ddbbabad4c6aef2e735a6f0ca5765df740a0
lucassai <lucasvmo42@gmail.com>
2026-01-27 16:38:51 -0300

    atualização 27/01

--- waybar/scripts/launch.sh
+++ waybar/scripts/launch.sh
@@ -1,16 +1,11 @@
-#!/bin/bash
-
-# Define o nome do processo que você quer controlar
 WAYBAR_PROCESS="waybar"
 
-# Verifica se o processo da Waybar está em execução
 if pgrep -x "$WAYBAR_PROCESS" > /dev/null
 then
-    # Se estiver em execução, ele mata o processo
     echo "Waybar está rodando. Matando o processo..."
     killall -9 "$WAYBAR_PROCESS"
 else
-    # Se não estiver em execução, ele inicia o processo em background
+
     echo "Waybar não está rodando. Iniciando..."
     "$WAYBAR_PROCESS" &
 fi
